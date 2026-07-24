7166e56bf0395f11d3831f2ce30b88b500e083a4
RakshitaBG <rakshitagudugudi@gmail.com>
2026-07-24 13:36:56 +0530

    3rd commmit

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,7 @@ final List<Map<String,dynamic>> foods=[
 
 
 {
-"name":"Grilled Chicken",
+"name":"Grilled Chicken 2",
 "description":"Juicy grilled chicken with special sauce",
 "price":18.99,
 "rating":"4.8",
