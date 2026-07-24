322cb74771346db0d5ad75c480d17876163bb882
RakshitaBG <rakshitagudugudi@gmail.com>
2026-07-24 13:35:21 +0530

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,257 +1,952 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const RestaurantApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+
+class RestaurantApp extends StatelessWidget {
+
+  const RestaurantApp({super.key});
+
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 236, 233, 233),
-        appBar: AppBar(
-          title: const Text('Restaurant App'),
-          centerTitle: true,
-          backgroundColor: Colors.deepOrange,
-          foregroundColor: Colors.white,
-          elevation: 0,
-        ),
-        body: const ResturantMenuPage(),
+
+      debugShowCheckedModeBanner:false,
+
+      theme:ThemeData(
+
+        primarySwatch:Colors.deepOrange,
+
+        scaffoldBackgroundColor:
+        const Color(0xfff8f8f8),
+
       ),
+
+      home:
+      const MainScreen(),
+
     );
   }
 }
 
-class ResturantMenuPage extends StatelessWidget {
-  const ResturantMenuPage({super.key});
+
+
+
+class MainScreen extends StatefulWidget {
+
+  const MainScreen({super.key});
+
 
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: ListView(
-        children: const [
-          Text(
-            "Today's Special",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: Colors.deepOrange,
-            ),
-          ),
-          SizedBox(height: 20),
+  State<MainScreen> createState()
+  => _MainScreenState();
 
-          MenuItemCard(
-            title: 'Grilled Salmon',
-            description:
-                'Freshly grilled salmon with lemon butter sauce.',
-            price: 18.99,
-            rating: 4.5,
-            tag: "Chef's Special",
-          ),
-
-          SizedBox(height: 20),
-
-          MenuItemCard(
-            title: 'Pasta Primavera',
-            description:
-                'Pasta with fresh vegetables and a light tomato sauce.',
-            price: 12.99,
-            rating: 4.2,
-            tag: 'Vegetarian',
-          ),
-
-          SizedBox(height: 20),
-
-          MenuItemCard(
-            title: 'Chocolate Lava Cake',
-            description:
-                'Warm chocolate cake with a gooey center.',
-            price: 6.99,
-            rating: 4.8,
-            tag: 'Dessert',
-          ),
-
-          SizedBox(height: 20),
-
-          MenuItemCard(
-            title: 'Caesar Salad',
-            description:
-                'Crisp romaine lettuce with Caesar dressing and croutons.',
-            price: 8.99,
-            rating: 4.0,
-            tag: 'Salad',
-          ),
-        ],
-      ),
-    );
-  }
-}
-class MenuItemCard extends StatefulWidget {
-  final String title;
-  final String description;
-  final double price;
-  final double rating;
-  final String tag;
-
-  const MenuItemCard({
-    super.key,
-    required this.title,
-    required this.description,
-    required this.price,
-    required this.rating,
-    required this.tag,
-  });
-
-  @override
-  State<MenuItemCard> createState() => _MenuItemCardState();
 }
 
-class _MenuItemCardState extends State<MenuItemCard> {
-  int quantity = 0;
 
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(vertical: 8),
-      elevation: 4,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              widget.title,
-              style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
 
-            const SizedBox(height: 8),
+class _MainScreenState extends State<MainScreen>{
 
-            Text(
-              widget.description,
-              style: const TextStyle(
-                fontSize: 16,
-                color: Colors.grey,
-              ),
-            ),
 
-            const SizedBox(height: 8),
+int selectedIndex=0;
 
-            Text(
-              "\$${widget.price.toStringAsFixed(2)}",
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Colors.deepOrange,
-              ),
-            ),
 
-            const SizedBox(height: 8),
+List<Map<String,dynamic>> cart=[];
 
-            Row(
-              children: [
-                const Icon(
-                  Icons.star,
-                  color: Colors.amber,
-                  size: 18,
-                ),
-                const SizedBox(width: 5),
-                Text(
-                  widget.rating.toStringAsFixed(1),
-                  style: const TextStyle(fontSize: 14),
-                ),
-              ],
-            ),
 
-            const SizedBox(height: 8),
 
-            Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 8,
-                vertical: 4,
-              ),
-              decoration: BoxDecoration(
-                color: Colors.deepOrange,
-                borderRadius: BorderRadius.circular(4),
-              ),
-              child: Text(
-                widget.tag,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
+final List<Map<String,dynamic>> foods=[
 
-            const SizedBox(height: 15),
 
-            Align(
-              alignment: Alignment.bottomRight,
-              child: quantity == 0
-                  ? ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.deepOrange,
-                        foregroundColor: Colors.white,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          quantity = 1;
-                        });
-                      },
-                      child: const Text("Order"),
-                    )
-                  : Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(
-                          icon: const Icon(
-                            Icons.remove_circle,
-                            color: Colors.deepOrange,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              if (quantity > 1) {
-                                quantity--;
-                              } else {
-                                quantity = 0;
-                              }
-                            });
-                          },
-                        ),
-                        Text(
-                          "$quantity",
-                          style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        IconButton(
-                          icon: const Icon(
-                            Icons.add_circle,
-                            color: Colors.deepOrange,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              quantity++;
-                            });
-                          },
-                        ),
-                      ],
-                    ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+{
+"name":"Grilled Chicken",
+"description":"Juicy grilled chicken with special sauce",
+"price":18.99,
+"rating":"4.8",
+"image":
+"https://images.unsplash.com/photo-1532550907401-a500c9a57435"
+},
+
+
+{
+"name":"Italian Pasta",
+"description":"Creamy pasta with vegetables",
+"price":12.99,
+"rating":"4.6",
+"image":
+"https://images.unsplash.com/photo-1473093295043-cdd812d0e601"
+},
+
+
+{
+"name":"Cheese Burger",
+"description":"Classic burger with cheese",
+"price":8.99,
+"rating":"4.7",
+"image":
+"https://images.unsplash.com/photo-1568901346375-23c9450c58cd"
+},
+
+
+{
+"name":"Chocolate Cake",
+"description":"Hot lava chocolate dessert",
+"price":6.99,
+"rating":"4.9",
+"image":
+"https://images.unsplash.com/photo-1578985545062-69928b1d9587"
+},
+
+
+];
+
+
+
+
+
+void addToCart(Map<String,dynamic> food){
+
+setState((){
+
+cart.add(food);
+
+});
+
+}
+
+
+
+
+List<Widget> get pages=>[
+
+
+homePage(),
+
+cartPage(),
+
+ordersPage(),
+
+profilePage(),
+
+];
+
+
+
+
+
+@override
+Widget build(BuildContext context){
+
+
+return Scaffold(
+
+
+body:
+pages[selectedIndex],
+
+
+
+bottomNavigationBar:
+
+
+
+Container(
+
+decoration:BoxDecoration(
+
+color:Colors.white,
+
+boxShadow:[
+
+BoxShadow(
+
+color:Colors.grey.shade300,
+
+blurRadius:15,
+
+)
+
+]
+
+),
+
+
+
+child:
+BottomNavigationBar(
+
+
+
+currentIndex:selectedIndex,
+
+
+type:
+BottomNavigationBarType.fixed,
+
+
+selectedItemColor:
+Colors.deepOrange,
+
+
+unselectedItemColor:
+Colors.grey,
+
+
+
+onTap:(index){
+
+setState((){
+
+selectedIndex=index;
+
+});
+
+},
+
+
+
+items:[
+
+
+
+const BottomNavigationBarItem(
+
+icon:
+Icon(Icons.home_outlined),
+
+activeIcon:
+Icon(Icons.home),
+
+label:"Home",
+
+),
+
+
+
+BottomNavigationBarItem(
+
+icon:
+Badge(
+
+label:
+Text(""),
+ 
+child:
+Icon(Icons.shopping_cart_outlined),
+
+),
+
+activeIcon:
+const Icon(Icons.shopping_cart),
+
+label:
+"Cart",
+
+),
+
+
+
+const BottomNavigationBarItem(
+
+icon:
+Icon(Icons.receipt_long_outlined),
+
+activeIcon:
+Icon(Icons.receipt_long),
+
+label:"Orders",
+
+),
+
+
+
+const BottomNavigationBarItem(
+
+icon:
+Icon(Icons.person_outline),
+
+activeIcon:
+Icon(Icons.person),
+
+label:"Profile",
+
+),
+
+
+
+],
+
+
+),
+
+),
+
+
+);
+
+
+}
+
+
+
+
+
+// ---------------- HOME PAGE ----------------
+
+
+Widget homePage(){
+
+
+return SafeArea(
+
+
+child:Padding(
+
+padding:
+const EdgeInsets.all(20),
+
+
+child:Column(
+
+crossAxisAlignment:
+CrossAxisAlignment.start,
+
+
+children:[
+
+
+
+Row(
+
+mainAxisAlignment:
+MainAxisAlignment.spaceBetween,
+
+
+children:[
+
+
+const Column(
+
+crossAxisAlignment:
+CrossAxisAlignment.start,
+
+
+children:[
+
+
+Text(
+
+"Hello Foodie 👋",
+
+style:
+TextStyle(
+
+color:Colors.grey,
+
+fontSize:16
+
+),
+
+),
+
+
+
+Text(
+
+"Find Your Favorite Food",
+
+style:
+TextStyle(
+
+fontSize:25,
+
+fontWeight:
+FontWeight.bold,
+
+),
+
+),
+
+
+
+],
+
+),
+
+
+
+CircleAvatar(
+
+backgroundColor:
+Colors.orange.shade100,
+
+
+child:
+const Icon(
+
+Icons.person,
+
+color:Colors.deepOrange,
+
+),
+
+)
+
+],
+
+
+),
+
+
+
+const SizedBox(height:25),
+
+
+
+Container(
+
+padding:
+const EdgeInsets.symmetric(
+horizontal:15
+),
+
+
+decoration:
+BoxDecoration(
+
+color:Colors.white,
+
+borderRadius:
+BorderRadius.circular(20),
+
+),
+
+
+child:
+const TextField(
+
+decoration:
+
+InputDecoration(
+
+hintText:"Search food",
+
+icon:
+Icon(Icons.search),
+
+border:
+InputBorder.none,
+
+),
+
+),
+
+),
+
+
+
+
+const SizedBox(height:20),
+
+
+
+const Text(
+
+"Popular Menu",
+
+style:
+TextStyle(
+
+fontSize:22,
+
+fontWeight:
+FontWeight.bold,
+
+),
+
+),
+
+
+
+const SizedBox(height:10),
+
+
+
+
+Expanded(
+
+child:
+ListView.builder(
+
+itemCount:
+foods.length,
+
+
+itemBuilder:(context,index){
+
+
+return foodCard(
+foods[index]
+);
+
+
+},
+
+
+),
+
+
+)
+
+],
+
+
+),
+
+
+),
+
+
+);
+
+
+}
+
+
+
+
+
+Widget foodCard(Map<String,dynamic> food){
+
+
+return Container(
+
+
+margin:
+const EdgeInsets.only(bottom:20),
+
+
+decoration:
+BoxDecoration(
+
+color:Colors.white,
+
+borderRadius:
+BorderRadius.circular(25),
+
+
+),
+
+
+
+child:Column(
+
+children:[
+
+
+
+ClipRRect(
+
+borderRadius:
+const BorderRadius.vertical(
+top:Radius.circular(25)
+),
+
+
+child:
+Image.network(
+
+food["image"],
+
+height:170,
+
+width:double.infinity,
+
+fit:BoxFit.cover,
+
+),
+
+),
+
+
+
+Padding(
+
+padding:
+const EdgeInsets.all(15),
+
+
+child:
+Column(
+
+crossAxisAlignment:
+CrossAxisAlignment.start,
+
+
+children:[
+
+
+Text(
+
+food["name"],
+
+style:
+const TextStyle(
+
+fontSize:20,
+
+fontWeight:
+FontWeight.bold,
+
+),
+
+),
+
+
+Text(
+
+food["description"],
+
+style:
+const TextStyle(
+color:Colors.grey
+),
+
+),
+
+
+
+Row(
+
+mainAxisAlignment:
+MainAxisAlignment.spaceBetween,
+
+
+children:[
+
+
+Text(
+
+"\$${food["price"]}",
+
+style:
+const TextStyle(
+
+fontSize:20,
+
+fontWeight:
+FontWeight.bold,
+
+color:Colors.deepOrange,
+
+),
+
+),
+
+
+
+ElevatedButton(
+
+style:
+ElevatedButton.styleFrom(
+
+backgroundColor:
+Colors.deepOrange,
+
+),
+
+
+onPressed:(){
+
+addToCart(food);
+
+},
+
+
+child:
+const Text(
+
+"Add",
+
+style:
+TextStyle(
+color:Colors.white
+),
+
+),
+
+)
+
+],
+
+
+)
+
+
+],
+
+
+),
+
+
+)
+
+
+],
+
+
+),
+
+
+);
+
+
+}
+
+
+
+
+
+// ---------------- CART PAGE ----------------
+
+
+Widget cartPage(){
+
+
+double total=0;
+
+
+for(var item in cart){
+
+total+=item["price"];
+
+}
+
+
+
+return Scaffold(
+
+appBar:AppBar(
+
+title:
+const Text("My Cart"),
+
+backgroundColor:
+Colors.deepOrange,
+
+),
+
+
+body:
+
+cart.isEmpty?
+
+
+const Center(
+
+child:
+Text(
+
+"Cart is empty",
+
+style:
+TextStyle(fontSize:22),
+
+),
+
+)
+
+
+:
+
+
+Column(
+
+children:[
+
+
+Expanded(
+
+child:
+ListView.builder(
+
+itemCount:
+cart.length,
+
+
+itemBuilder:(context,index){
+
+
+return ListTile(
+
+leading:
+Image.network(
+
+cart[index]["image"],
+
+width:60,
+
+fit:BoxFit.cover,
+
+),
+
+
+title:
+Text(cart[index]["name"]),
+
+
+trailing:
+Text(
+"\$${cart[index]["price"]}"
+),
+
+
+);
+
+},
+
+
+),
+
+),
+
+
+
+Padding(
+
+padding:
+const EdgeInsets.all(20),
+
+child:
+Text(
+
+"Total : \$${total.toStringAsFixed(2)}",
+
+style:
+const TextStyle(
+
+fontSize:22,
+
+fontWeight:
+FontWeight.bold,
+
+),
+
+),
+
+)
+
+],
+
+
+),
+
+
+);
+
+
+}
+
+
+
+
+
+Widget ordersPage(){
+
+
+return const Center(
+
+child:
+Text(
+
+"No Orders Yet 📦",
+
+style:
+TextStyle(
+
+fontSize:25,
+
+fontWeight:FontWeight.bold,
+
+),
+
+),
+
+);
+
+
+}
+
+
+
+
+
+Widget profilePage(){
+
+
+return const Center(
+
+child:
+Column(
+
+mainAxisAlignment:
+MainAxisAlignment.center,
+
+children:[
+
+
+CircleAvatar(
+
+radius:50,
+
+child:
+Icon(
+
+Icons.person,
+
+size:50,
+
+),
+
+),
+
+
+
+SizedBox(height:20),
+
+
+
+Text(
+
+"Food Lover",
+
+style:
+TextStyle(
+
+fontSize:25,
+
+fontWeight:
+FontWeight.bold,
+
+),
+
+)
+
+
+],
+
+
+),
+
+);
+
+
+}
+
+
+
 }
